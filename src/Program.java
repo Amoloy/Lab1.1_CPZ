bb4e70013a11a063e91da774f74e022e6cd1a11c
Nick Kruts <Mykola.Kryc@livenau.net>
2020-12-14 21:39:41 +0200

    'program_update1_amoloy'

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,16 @@
 public class Program {
     public static void main(String args[]) {
         try {
-            Cashier_Bank cashier = new Cashier_Bank();
-            Director_Bank director = new Director_Bank();
+            Cashier_Bank cashier = new Cashier_Bank(100);
+            Director_Bank director = new Director_Bank(4000);
 
-            Bank_Employee employee1 = new Bank_Employee("Petro", "Dyshlevyy", "Male", 30, director);
-            Bank_Employee employee2 = new Bank_Employee("Stepa", "Duryk", "Male", 21, cashier);
-//            Bank_Employee employee3 = new Bank_Employee("Noname", "Gnyda", "Male", 13, cashier);
+            Bank_Employee employee3 = new Bank_Employee("Zhack", "Fresko", "Male", 101,  cashier);
+            Bank_Employee employee2 = new Bank_Employee("Petro", "Dyshlevyy", "Male", 40, director);
+            Bank_Employee employee1 = new Bank_Employee("Stepa", "Neponyatnych", "Male", 21, cashier);
 
             System.out.println(employee1.Show_Post_Info());
             System.out.println(employee2.Show_Post_Info());
-//            System.out.println(employee3.Show_Post_Info());
+            System.out.println(employee3.Show_Post_Info());
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
